144e6dccab057ff790731d44aeb07f59d4d6d1f6
oalsaab <omaralsaab96@gmail.com>
2021-12-18 13:10:06 +0000

    Added comments to code. Unpacked tuple return value in reddit_main without use of indicies

--- reddit_main.py
+++ reddit_main.py
@@ -2,7 +2,7 @@ from reddit_funcs import request_subreddit, time_created, request_subreddit_info
 from tabulate import tabulate
 
 
-# function that keeps requesting integer as value
+# Function that keeps requesting integer as value
 def input_minutes(request_minutes):
     while True:
         try:
@@ -19,13 +19,16 @@ subreddits = [subreddit for subreddit in input('Enter subreddit(s): ').split()]
 minutes_input = input_minutes('Enter in minutes timeframe: ')
 
 main_list = []
+# Loops over all the subreddits provided by user and performs the functions on each subreddit
 for subreddit in subreddits:
     request_subreddit(subreddit)
-    subscribers = request_subreddit_info(subreddit)[0]
-    accounts_active = request_subreddit_info(subreddit)[1]
-    posts = time_created(subreddit, minutes_input)[0]
-    comments = time_created(subreddit, minutes_input)[1]
+    
+    subscribers, accounts_active = request_subreddit_info(subreddit)
+    
+    posts, comments = time_created(subreddit, minutes_input)
+    
     response_list = [subreddit, subscribers, accounts_active, posts, comments]
+    
     main_list.append(response_list)
 
 columns = ['subreddit', 'subscribers', 'accounts_active', 'posts', 'comments']
